950f616c7b95076d9a93cd5aa43d81348c51bc35
russellgoldenberg <russellgoldenberg@gmail.com>
2020-03-19 17:53:00 -0400

    connect audio toggles

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -2,18 +2,31 @@
 import jump from 'jump.js';
 
 const $intro = d3.select('#intro');
+const $header = d3.select('header');
 
 function resize() {}
 
-function handleIntroButton() {
-  const audio = d3.select(this).attr('data-audio');
-  const over = audio === 'on' ? 'on. Good choice.' : 'off.';
+function toggleAudio(dir) {
+  const isOn = $header.select('.audio .on').classed('is-visible');
+  const goOn = dir ? dir === 'on' : !isOn;
+  const over = goOn ? 'on. Good choice.' : 'off.';
   $intro.select('.intro__overline span').text(over);
+  $header.select('.on').classed('is-visible', goOn);
+  $header.select('.off').classed('is-visible', !goOn);
+}
+function handleHeader() {
+  toggleAudio();
+}
+
+function handleIntro() {
+  const audio = d3.select(this).attr('data-audio');
+  toggleAudio(audio);
   jump('article', { duration: 500 });
 }
 
 function init() {
-  $intro.selectAll('button').on('click', handleIntroButton);
+  $intro.selectAll('button').on('click', handleIntro);
+  d3.select('.audio').on('click', handleHeader);
 }
 
 export default { init, resize };
