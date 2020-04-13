193711c11cdf7fc2df23b57e33a36e2a9168e317
russellgoldenberg <russellgoldenberg@gmail.com>
2020-04-13 13:31:26 -0400

    archive

--- src/js/results.js
+++ src/js/results.js
@@ -63,7 +63,7 @@ function init({ levels }) {
     .attr('data-title', 'Est. Completion')
     .text(d => {
       if (d.result && d.result.done) return `Finished ${getDate(d.result.end)}`;
-      if (d.result) return d.estimate;
+      if (d.result && d.estimate === 'Anytime now...') return d.estimate;
       return `In ${d.estimate}`;
     });
 
